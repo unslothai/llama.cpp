1437194e28cce7814872381fa65b0fc7c537bd02
Daniel Han <danielhanchen@gmail.com>
2025-08-05 18:40:13 -0700

    Update convert_hf_to_gguf.py

--- convert_hf_to_gguf.py
+++ convert_hf_to_gguf.py
@@ -8066,7 +8066,7 @@ class GptOssModel(TextModel):
                 name_gate = name.replace("gate_up_proj", "gate_proj.weight")
                 dim_half = data_torch.shape[-1] // 2
                 print("#### dim_half = ", dim_half)
-                gate_proj_weight, up_proj_weight = data_torch[..., ::2].transpose(-1, -2), data_torch[..., 1::2].transpose(-1, -2)
+                gate_proj_weight, up_proj_weight = data_torch.transpose(-1, -2).split(dim_half, dim=-2)
                 return [
                     (self.map_tensor_name(name_gate), gate_proj_weight),
                     (self.map_tensor_name(name_up), up_proj_weight)
