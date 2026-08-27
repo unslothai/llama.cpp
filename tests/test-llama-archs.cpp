eaf93765572e794b8e3754fe45adbe12d381e997
Xuan Son Nguyen <son@huggingface.co>
2026-08-27 21:28:11 +0200

    exclude from webgpu test

--- tests/test-llama-archs.cpp
+++ tests/test-llama-archs.cpp
@@ -517,7 +517,7 @@ static bool arch_supported(const llm_arch arch) {
     }
     // FIXME: these hit scheduler/view-backed-output issues with WebGPU on CI.
 #ifdef GGML_USE_WEBGPU
-    if (arch == LLM_ARCH_DEEPSEEK32 || arch == LLM_ARCH_GLM_DSA || arch == LLM_ARCH_DOTS3NOTE) {
+    if (arch == LLM_ARCH_DEEPSEEK32 || arch == LLM_ARCH_GLM_DSA || arch == LLM_ARCH_DOTS3NOTE || arch == LLM_ARCH_QWEN4EXP) {
         return false;
     }
 #endif // GGML_USE_WEBGPU
